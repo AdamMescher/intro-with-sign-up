d2be244ef0341b56d592ee5ccc23cf737aef5937
Adam Mescher <admescher@gmail.com>
2022-07-20 15:52:46 -0600

    Update App component styles and update section styled component names to be more descriptive

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,35 +2,41 @@ import * as React from 'react';
 import styled from 'styled-components';
 import Banner from '../Banner';
 import SignUpForm from '../SignUpForm';
+import Title from '../Title';
 import backgroundMobile from '../../assets/bg-intro-mobile.png'; /////
 import backgroundDesktop from '../../assets/bg-intro-desktop.png'; /////
 
 function App() {
   return (
     <Wrapper data-testid='app'>
-      <Section>
-        <h1>Learn to code by watching others</h1>
+      <Copy>
+        <Title as={'h1'}>Learn to code by watching others</Title>
         <p>
           See how experienced developers solve problems in real-time. Watching
           scripted tutorials is great, but understanding how developers think is
           invaluable.
         </p>
-      </Section>
-      <Section>
+      </Copy>
+      <Form>
         <Banner>
           <p>
             <Bold>Try it free 7 days</Bold> {` then $20/mo. thereafter`}
           </p>
         </Banner>
         <SignUpForm />
-      </Section>
+      </Form>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.main`
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding-top: 74px;
+  padding-bottom: 74px;
+  gap: 24px;
   background-color: var(--color-primary-red);
   background-image: url(${backgroundMobile});
   background-image: -webkit-image-set(
@@ -41,6 +47,9 @@ const Wrapper = styled.main`
     url(${backgroundMobile}) 1x,
     url(${backgroundDesktop}) 2x
   );
+  h1 {
+    font-weight: var(--font-weight-semi-bold);
+  }
 `;
 const Bold = styled.span`
   font-weight: var(--font-weight-bold);
@@ -48,7 +57,22 @@ const Bold = styled.span`
 
 const Section = styled.section`
   width: 100%;
-  border: 2px solid magenta;
+  width: clamp(320px, 95%, 1280px);
+  padding-left: 24px;
+  padding-right: 24px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Copy = styled(Section)`
+  color: var(--color-neutral-white);
+  text-align: center;
+`;
+
+const Form = styled(Section)`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
 `;
 
 export default App;
